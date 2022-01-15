4b4f539ebae6101ebd07b0cdb4e2cde6484b7eed
kylerparker <38117449+kylerparker@users.noreply.github.com>
2022-01-14 18:18:23 -0600

    UI Test
    Basic navigation though app. Added app logo and splash page on load.

---
+++ app/src/main/java/com/example/caravantest/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.example.caravantest.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is where maps will be shown.");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/example/caravantest/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.caravantest.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is where group functionality will take place");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
