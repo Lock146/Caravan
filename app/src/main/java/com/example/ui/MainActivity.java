d28c0f0c08ac9ac91be13890b6270a200013b3f1
colbyboyd410 <56280845+colbyboyd410@users.noreply.github.com>
2022-02-23 17:10:12 -0600

    This should bring up sign in screen and load google sign in only

--- app/src/main/java/com/example/ui/MainActivity.java
+++ app/src/main/java/com/example/ui/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.ui;
 
-import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,8 +10,13 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.ui.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<actionCodeSettings> extends AppCompatActivity {
+    //FirebaseAuth mAuth;
 
     private ActivityMainBinding binding;
 
@@ -32,6 +36,20 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        //mAuth = FirebaseAuth.getInstance();
+
+    }
+    public void getGoogleCredentials() {
+        String googleIdToken = "";
+        // [START auth_google_cred]
+        AuthCredential credential = GoogleAuthProvider.getCredential(googleIdToken, null);
+        // [END auth_google_cred]
+
+    }
+    public void signOut() {
+        // [START auth_sign_out]
+        FirebaseAuth.getInstance().signOut();
+        // [END auth_sign_out]
     }
 
-}
+ }
