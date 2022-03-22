569b0645b32c6559152ef3b0d190499fba40b372
colbyboyd410 <56280845+colbyboyd410@users.noreply.github.com>
2022-03-22 12:59:48 -0500

    Chat Screen Layout for received and sent messages

---
+++ app/src/main/java/com/example/caravantest/GroupChat.java
@@ -0,0 +1,14 @@
+package com.example.caravantest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GroupChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+    }
+}
