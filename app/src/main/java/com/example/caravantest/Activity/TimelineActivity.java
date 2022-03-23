093631d7677e4f4767e10e5bf24fe8d8381f8515
dwunst <djwunstel@gmail.com>
2022-03-23 14:24:11 -0500

    Added ability to swap different destinations on the Timeline

--- app/src/main/java/com/example/caravantest/Activity/TimelineActivity.java
+++ app/src/main/java/com/example/caravantest/Activity/TimelineActivity.java
@@ -1,15 +1,19 @@
 package com.example.caravantest.Activity;
 
+import android.media.MediaRouter;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.caravantest.Adapter.TimelineAdapter;
 import com.example.caravantest.R;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TimelineActivity extends AppCompatActivity {
@@ -25,6 +29,11 @@ public class TimelineActivity extends AppCompatActivity {
 
         destiList = new ArrayList<>();
 
+        //TODO: Create a function that takes the name of a destination
+        // on a route and input it onto the list
+        destiList.add("New Orleans");
+        destiList.add("Ruston");
+
         recyclerView = findViewById(R.id.recyclerView);
         timelineAdapter = new TimelineAdapter(destiList);
 
@@ -33,9 +42,29 @@ public class TimelineActivity extends AppCompatActivity {
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
         recyclerView.addItemDecoration(dividerItemDecoration);
 
-        //TODO: Create a function that takes the name of a destination
-        // on a route and input it onto the list
-        destiList.add("New Orleans");
-        destiList.add("Ruston");
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+
     }
+
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP |ItemTouchHelper.DOWN| ItemTouchHelper.START| ItemTouchHelper.END, 0 ) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            int fromPosition = viewHolder.getAdapterPosition();
+            int toPosition = target.getAdapterPosition();
+
+            Collections.swap(destiList, fromPosition, toPosition);
+
+            recyclerView.getAdapter().notifyItemMoved(fromPosition, toPosition);
+
+
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+        }
+    };
 }
