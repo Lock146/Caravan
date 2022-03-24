4e9c043ff737584bcde4078982586195ef269ddf
Lock146 <32308863+Lock146@users.noreply.github.com>
2022-03-24 14:52:30 -0500

    Added a button that automatically crashes the app on press. (a feature)

--- app/src/main/java/com/example/caravantest/Activity/SplashActivity.java
+++ app/src/main/java/com/example/caravantest/Activity/SplashActivity.java
@@ -3,7 +3,6 @@ package com.example.caravantest.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.caravantest.R;
