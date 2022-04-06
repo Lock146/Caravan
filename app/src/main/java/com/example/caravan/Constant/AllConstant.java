759f7ab5993b386e3aaaca2333da78b2f1ca23cd
Lock146 <32308863+Lock146@users.noreply.github.com>
2022-04-06 16:34:30 -0500

    Added icons to recommended

--- app/src/main/java/com/example/caravan/Constant/AllConstant.java
+++ app/src/main/java/com/example/caravan/Constant/AllConstant.java
@@ -17,15 +17,15 @@ public interface AllConstant {
 
     ArrayList<PlaceModel> placesName = new ArrayList<>(
             Arrays.asList(
-                    new PlaceModel(1, R.drawable.ic_gas_station, "Restaurant", "restaurant"),
-                    new PlaceModel(2, R.drawable.ic_gas_station, "ATM", "atm"),
-                    new PlaceModel(3, R.drawable.ic_gas_station, "Gas", "gas_station"),
-                    new PlaceModel(4, R.drawable.ic_gas_station, "Groceries", "supermarket"),
-                    new PlaceModel(5, R.drawable.ic_gas_station, "Hotels", "hotel"),
-                    new PlaceModel(6, R.drawable.ic_gas_station, "Attractions", "tourist_attraction"),
-                    new PlaceModel(7, R.drawable.ic_gas_station, "Parks", "park"),
-                    new PlaceModel(8, R.drawable.ic_gas_station, "Museums", "museum"),
-                    new PlaceModel(9, R.drawable.ic_gas_station, "Bars", "bar")
+                    new PlaceModel(1, R.drawable.ic_food, "Restaurant", "restaurant"),
+                    new PlaceModel(2, R.drawable.ic_atm, "ATM", "atm"),
+                    new PlaceModel(3, R.drawable.ic_gas, "Gas", "gas_station"),
+                    new PlaceModel(4, R.drawable.ic_groceries, "Groceries", "supermarket"),
+                    new PlaceModel(5, R.drawable.ic_hotel, "Hotels", "hotel"),
+                    new PlaceModel(6, R.drawable.ic_attractions, "Attractions", "tourist_attraction"),
+                    new PlaceModel(7, R.drawable.ic_parks, "Parks", "park"),
+                    new PlaceModel(8, R.drawable.ic_museums, "Museums", "museum"),
+                    new PlaceModel(9, R.drawable.ic_bars, "Bars", "bar")
 
             )
     );
