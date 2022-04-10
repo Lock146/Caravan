dd60d1063973d6629cc4270d8c07ffa47bebde55
clm101 <62450267+clm101@users.noreply.github.com>
2022-04-09 22:34:01 -0500

    Update RouteTimelineActivity with StopInfo refactor

--- app/src/main/java/com/example/caravan/Activity/RouteTimelineActivity.java
+++ app/src/main/java/com/example/caravan/Activity/RouteTimelineActivity.java
@@ -20,60 +20,26 @@ import java.util.Collections;
 import java.util.List;
 
 public class RouteTimelineActivity extends AppCompatActivity {
-
-
-    RecyclerView recyclerView;
-    RouteTimelineAdapter routeTimelineAdapter;
-
-    //List<String> Routes;
-    //List<Double> Miles;
-
-    List<StopInfo> CurrentRoute;
+    private RecyclerView recyclerView;
+    private RouteTimelineAdapter routeTimelineAdapter;
+    private List<StopInfo> CurrentRoute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routetimeline);
 
-
-        //Routes = new ArrayList<>();
-        //Miles = new ArrayList<>();
-
         CurrentRoute = new ArrayList<>();
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setFitsSystemWindows(true);
-        //recyclerAdapter = new RecyclerAdapter(Routes,Miles);
         routeTimelineAdapter = new RouteTimelineAdapter(CurrentRoute);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         recyclerView.setAdapter(routeTimelineAdapter);
 
-
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
         recyclerView.addItemDecoration(dividerItemDecoration);
-
-
-        CurrentRoute.add(new StopInfo("Ruston",1609.344));
-        CurrentRoute.add(new StopInfo("New Orleans", 160934.000));
-
-
-       /* Routes.add("Ruston");
-        Miles.add(1609.344);
-        Routes.add("New Orleans");
-        Miles.add(160934.000);
-        Routes.add("Disney World");
-        Miles.add(32186.98);
-        Routes.add("Chicago");
-        Miles.add(22530.88);
-        Routes.add("London");
-        Miles.add(320259.00);
-        Routes.add("Houston");
-        Miles.add(85285.22);
-        */
-
-
-
 
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
         itemTouchHelper.attachToRecyclerView(recyclerView);
@@ -85,53 +51,33 @@ public class RouteTimelineActivity extends AppCompatActivity {
             int fromPosition = viewHolder.getAdapterPosition();
             int toPosition = target.getAdapterPosition();
 
-            //Collections.swap(Routes,fromPosition,toPosition);
-            //Collections.swap(Miles,fromPosition,toPosition);
             Collections.swap(CurrentRoute,fromPosition,toPosition);
 
             recyclerView.getAdapter().notifyItemMoved(fromPosition,toPosition);
 
-
             return false;
         }
-
-
-        //String deletedRoute = null;
-        //double deletedMiles = 0.00;
         StopInfo deletedRoute;
 
         @Override
         public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
             int position = viewHolder.getAdapterPosition();
 
-
-
-            //deletedRoute = Routes.get(position);
-            //deletedMiles = Miles.get(position);
-
             deletedRoute = CurrentRoute.get(position);
-
-
-            //Routes.remove(position);
-            //Miles.remove(position);
 
             CurrentRoute.remove(position);
 
             routeTimelineAdapter.notifyItemRemoved(position);
-            Snackbar.make(recyclerView, deletedRoute.getRouteName(), Snackbar.LENGTH_LONG)
+            Snackbar.make(recyclerView, deletedRoute.name(), Snackbar.LENGTH_LONG)
                     .setAction("Undo", new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
-
-                            //Routes.add(position, deletedRoute);
-                            //Miles.add(position, deletedMiles);
                             CurrentRoute.add(position, deletedRoute);
 
                             routeTimelineAdapter.notifyItemInserted(position);
                         }
                     }).show();
-
             }
         };
-    };
+    }
 
