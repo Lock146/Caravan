175b98f906647e6d051cb72c6eabd0a0d847f18f
colbyboyd410 <56280845+colbyboyd410@users.noreply.github.com>
2022-04-11 11:55:31 -0500

    created API client

---
+++ app/src/main/java/com/example/caravan/network/ApiClient.java
@@ -0,0 +1,14 @@
+package com.example.caravan.network;
+
+import retrofit2.Retrofit;
+
+public class ApiClient {
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("https")
+        }
+    }
+}
