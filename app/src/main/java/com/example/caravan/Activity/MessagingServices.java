44be3d8db00e63f64b7ef962bae4bfcc64dc7602
colbyboyd410 <56280845+colbyboyd410@users.noreply.github.com>
2022-04-12 13:08:54 -0500

    changes to messaging services file

--- app/src/main/java/com/example/caravan/Activity/MessagingServices.java
+++ app/src/main/java/com/example/caravan/Activity/MessagingServices.java
@@ -28,55 +28,14 @@ import java.util.Map;
 
 public class MessagingServices extends FirebaseMessagingService {
 
-    private static final String TAG = "MyFirebaseMsgService";
-
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d(TAG, "Message: " + remoteMessage.getFrom());
+    public void onNewToken(@NonNull String token){
+        super.onNewToken(token);
+    Log.d("FCM", "Token: " + token); }
 
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData()); }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-    }
-    @Override
-    public void onNewToken(String token) {
-        Log.d(TAG, "Refreshed token: " + token);
-        // Sends the FCM registration token to app server.
-        sendRegistrationToServer(token);
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage){
+        super.onMessageReceived(remoteMessage);
+        Log.d("FCM", "Message: " + remoteMessage.getNotification().getBody());
     }
 
-    private void sendRegistrationToServer(String token) {
-
-    }
-
-    private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-
-        String channelId = getString(R.string.default_notification_channel_id);
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.drawable.ic_notifications)
-                        .setContentTitle(getString(R.string.fcm_message))
-                        .setContentText(messageBody)
-                        .setAutoCancel(true)
-                        .setSound(defaultSoundUri)
-                        .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(channelId,
-                    "Channel human readable title",
-                    NotificationManager.IMPORTANCE_HIGH); }
-        }
-}
+   }
---
+++ app/src/main/java/com/example/caravan/network/ApiServer.java
@@ -0,0 +1,4 @@
+package com.example.caravan.network;
+
+public class ApiServer {
+}
