1343abf509c77b802f8c37cd4a79b6f71b24ccd7
colbyboyd410 <colbyb9201@gmail.com>
2022-04-24 17:03:19 -0500

    api client and service file completed

--- app/src/main/java/com/example/caravan/network/ApiClient.java
+++ app/src/main/java/com/example/caravan/network/ApiClient.java
@@ -1,15 +1,19 @@
 package com.example.caravan.network;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class ApiClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
-        if (retrofit == null){
-//            retrofit = new Retrofit.Builder()
-//                    .baseUrl("https");
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("https://fcm.googleapis.com/fcm/")
+                    .addConverterFactory(ScalarsConverterFactory.create())
+                    .build();
         }
-        return null;
+        return retrofit;
     }
 }
--- app/src/main/java/com/example/caravan/network/ApiServer.java
+++
@@ -1,4 +0,0 @@
-package com.example.caravan.network;
-
-public class ApiServer {
-}
