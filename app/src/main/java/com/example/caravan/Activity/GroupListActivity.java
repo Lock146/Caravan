e1ebe93414ef6e9c69805e4ec60d83160aed6379
dwunstel <djwunstel@gmail.com>
2022-04-26 09:53:30 -0500

    Added dividers to the group list and a testing toast

--- app/src/main/java/com/example/caravan/Activity/GroupListActivity.java
+++ app/src/main/java/com/example/caravan/Activity/GroupListActivity.java
@@ -3,11 +3,15 @@ package com.example.caravan.Activity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.caravan.Adapter.GroupListAdapter;
 import com.example.caravan.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupListActivity extends AppCompatActivity
 {
@@ -16,18 +20,37 @@ public class GroupListActivity extends AppCompatActivity
     private RecyclerView recyclerView;
     private GroupListAdapter groupListAdapter;
 
+    private List<String> groupMember;
+
      @Override
     protected void onCreate(Bundle savedInstanceState){
          super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_grouplist);
 
+         groupMember = new ArrayList<>();
+
          recyclerView = findViewById(R.id.recyclerView2);
-
-         groupListAdapter = new GroupListAdapter();
-
+         groupListAdapter = new GroupListAdapter(groupMember);
          recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
          recyclerView.setAdapter(groupListAdapter);
+
+         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+         recyclerView.addItemDecoration(dividerItemDecoration);
+
+
+         groupMember.add("Cameron");
+         groupMember.add("Luke");
+         groupMember.add("Colby");
+         groupMember.add("Kyler");
+         groupMember.add("Cameron");
+         groupMember.add("Luke");
+         groupMember.add("Colby");
+         groupMember.add("Kyler");
+
+         groupMember.add("Cameron");
+         groupMember.add("Luke");
+         groupMember.add("Colby");
+         groupMember.add("Kyler");
 
 
      }
--- app/src/main/java/com/example/caravan/Adapter/GroupListAdapter.java
+++ app/src/main/java/com/example/caravan/Adapter/GroupListAdapter.java
@@ -6,15 +6,22 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.caravan.R;
 
+import java.util.List;
+
 public class GroupListAdapter  extends RecyclerView.Adapter<GroupListAdapter.ViewHolder> {
 
+    List<String> groupMembers;
 
+    public GroupListAdapter(List<String> groupMembers) {
+        this.groupMembers = groupMembers;
+    }
 
     @NonNull
     @Override
@@ -29,18 +36,21 @@ public class GroupListAdapter  extends RecyclerView.Adapter<GroupListAdapter.Vie
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText(groupMembers.get(position));
+        holder.blank.setText("");
 
+        holder.imageView.setImageResource(R.mipmap.ic_emoji_13_round);
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return groupMembers.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView imageView;
-        TextView textView;
+        TextView textView,blank;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -49,6 +59,15 @@ public class GroupListAdapter  extends RecyclerView.Adapter<GroupListAdapter.Vie
             imageView = itemView.findViewById(R.id.imageView);
 
             textView = itemView.findViewById(R.id.textView);
+            blank = itemView.findViewById(R.id.rowCountTextView);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) 
+        {
+            Toast.makeText(view.getContext(), "clicked", Toast.LENGTH_SHORT).show();
         }
     }
 }
