088a653dfe3e2608d5841c59c1350a5cada7a27f
Cameron Mitchell <clm8043@gmail.com>
2022-04-25 22:20:26 -0500

    Removed redundant information

--- app/src/main/java/com/example/caravan/Activity/GroupChatActivity.java
+++ app/src/main/java/com/example/caravan/Activity/GroupChatActivity.java
@@ -88,8 +88,6 @@ public class GroupChatActivity extends AppCompatActivity {
                 if(change.getType() == DocumentChange.Type.ADDED) {
                     ChatMessage message = new ChatMessage();
                     message.senderId = change.getDocument().getString(Constants.KEY_SENDER_ID);
-                    message.email = change.getDocument().getString("displayName");
-                    message.image = change.getDocument().getString("profilePicture");
                     message.message = change.getDocument().getString(Constants.KEY_MESSAGE);
                     message.dateTime = getReadableDateTime(change.getDocument().getDate(Constants.KEY_TIMESTAMP));
                     message.dateObject = change.getDocument().getDate(Constants.KEY_TIMESTAMP);
