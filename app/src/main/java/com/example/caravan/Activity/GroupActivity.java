582d2d36b9a50aec7a02a28ab8aeff757d2d02f9
Cameron Mitchell <clm8043@gmail.com>
2022-05-05 13:51:21 -0500

    Clean up

--- app/src/main/java/com/example/caravan/Activity/GroupActivity.java
+++ app/src/main/java/com/example/caravan/Activity/GroupActivity.java
@@ -63,8 +63,6 @@ public class GroupActivity extends AppCompatActivity {
         recyclerView.addItemDecoration(dividerItemDecoration);
 
         setListeners();
-        //binding.groupMember.setOnClickListener(view -> add_user());
-        //binding.chat.setOnClickListener(view -> open_group_chat());
 
         binding.GroupName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -75,33 +73,6 @@ public class GroupActivity extends AppCompatActivity {
             }
         });
     }
-    private void leave_group(){
-        Database.get_instance().leave_group();
-        binding.chat.setText(CREATE_GROUP);
-        binding.chat.setOnClickListener(view -> {
-            create_group();
-        });
-
-//        binding.groupMembership.setVisibility(View.INVISIBLE);
-//        binding.groupMembership.setClickable(false);
-        disable_group_functionality();
-    }
-
-    private void create_group(){
-        Database.get_instance().create_group();
-        //enable_group_functionality();
-    }
-
-    private void add_user(){
-//        if(!binding.addEmail.getText().toString().isEmpty()) {
-//            String email = binding.addEmail.getText().toString();
-//            binding.addEmail.setText(null);
-//            Database.get_instance().add_user(email);
-//        }
-//        else{
-//            Toast.makeText(this, "Must provide email", Toast.LENGTH_SHORT).show();
-//        }
-    }
 
     private void open_group_chat(){
         Log.d(TAG, "open_group_chat called");
@@ -110,38 +81,6 @@ public class GroupActivity extends AppCompatActivity {
     private void go_to_group(){
         startActivity(new Intent(this, GroupMembersActivity.class));
     }
-    private void disable_group_functionality(){
-        //binding.addUser.setText(CREATE_GROUP);
-        //binding.addUser.setOnClickListener(view -> {
-            create_group();
-        }//);
-
-        //binding.chat.setVisibility(View.INVISIBLE);
-        //binding.chat.setClickable(false);
-        //binding.groupMembership.setVisibility(View.INVISIBLE);
-        //binding.groupMembership.setClickable(false);
-//        binding.groupMembership.setVisibility(View.INVISIBLE);
-//        binding.groupMembership.setClickable(false);
-//
-//        binding.groupList.setVisibility(View.INVISIBLE);
-//        binding.groupList.setClickable(false);
-
-
-//    private void enable_group_functionality(){
-//        //binding.addUser.setText(ADD_USER);
-//        //binding.addUser.setOnClickListener(view -> {
-//        //    add_user();
-//        //});
-
-        //binding.chat.setVisibility(View.VISIBLE);
-        //binding.chat.setClickable(true);
-        //binding.groupMembership.setVisibility(View.VISIBLE);
-        //binding.groupMembership.setClickable(true);
-        //binding.groupMembership.setVisibility(View.VISIBLE);
-        //binding.groupMembership.setClickable(true);
-        //binding.groupList.setVisibility(View.VISIBLE);
-        //binding.groupList.setClickable(true);
-    //}
 
     private void open_list(){
         startActivity(new Intent(this, GroupListActivity.class));
